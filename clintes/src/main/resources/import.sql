623efb9bcf00b69c89c5a317694ff43a4512802c
alex cunha <alexcunhadev@gmail.com>
2022-06-18 12:05:01 -0300

    create entity, dto, seeting

---
+++ clintes/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ("Alex Maia", 2000.00,  "1992-06-09T10:30:00Z");
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ("Marceli", 3000.00,  "1996-05-12T10:30:00Z");
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ("Marceli", 3000.00,  "1996-05-12T10:30:00Z");
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ("Marceli", 3000.00,  "1996-05-12T10:30:00Z");
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ("Marceli", 3000.00,  "1996-05-12T10:30:00Z");
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ("Marceli", 3000.00,  "1996-05-12T10:30:00Z");
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ("Marceli", 3000.00,  "1996-05-12T10:30:00Z");
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ("Marceli", 3000.00,  "1996-05-12T10:30:00Z");
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ("Marceli", 3000.00,  "1996-05-12T10:30:00Z");
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ("Marceli", 3000.00,  "1996-05-12T10:30:00Z");
